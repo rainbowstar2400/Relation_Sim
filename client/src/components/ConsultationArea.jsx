149603278a25f1a569acc9bbbb6bfc85397e6e59
優 <166690318+rainbowstar2400@users.noreply.github.com>
2025-07-19 00:42:58 +0900

    ログ表示タイミングを対応ボタンに変更

--- client/src/components/ConsultationArea.jsx
+++ client/src/components/ConsultationArea.jsx
@@ -38,7 +38,6 @@ export default function ConsultationArea({ characters, trusts, updateTrust, addL
         const timeout = setTimeout(() => {
           setConsultations(p => p.filter(c => c.id !== id))
         }, AUTO_INTERVAL_MS)
-        addLog(`${char.name}がプレイヤーに相談しています…`)
         updateLastConsultation(char.id)
         return [...prev, { id, char, template, timeout }]
       })
@@ -60,7 +59,6 @@ export default function ConsultationArea({ characters, trusts, updateTrust, addL
     }, AUTO_INTERVAL_MS)
     const c = { id, char, template, timeout }
     setConsultations(prev => [...prev, c])
-    addLog(`${char.name}がプレイヤーに相談しています…`)
     updateLastConsultation(char.id)
   }
 
@@ -68,6 +66,7 @@ export default function ConsultationArea({ characters, trusts, updateTrust, addL
     setCurrent(c)
     setSelected('')
     setAnswered(false)
+    addLog(`${c.char.name}がプレイヤーに相談しています…`)
   }
 
   // 回答を送信
