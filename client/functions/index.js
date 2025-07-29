bb85f211c0090a20290db1aa6296446f3983af25
rainbowstar2400 <rainbowstar2400@gmail.com>
2025-07-29 10:39:48 +0900

    Fix

--- client/functions/index.js
+++ client/functions/index.js
@@ -8,35 +8,22 @@
  */
 
 const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
+// 変更点 1: functionsモジュール全体を読み込む
+const functions = require("firebase-functions");
 const logger = require("firebase-functions/logger");
 const admin = require("firebase-admin");
 
 admin.initializeApp();
 const db = admin.firestore();
 
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
+// For cost control...
 setGlobalOptions({ maxInstances: 10 });
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// ...
 
 // Cloud Scheduler から 15 分ごとに呼び出される予定の関数
-exports.generateAutoEvents = onRequest(async (_req, res) => {
+// 変更点 2: .region("asia-northeast1") を追加
+exports.generateAutoEvents = functions.region("asia-northeast1").https.onRequest(async (_req, res) => {
   try {
     const users = await db.collection("saves").get();
     const timestamp = Date.now();
