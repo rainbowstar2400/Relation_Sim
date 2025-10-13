1828c35e966b322fda15a5fe3299583efc088cdc
優 <166690318+rainbowstar2400@users.noreply.github.com>
2025-10-13 18:27:19 +0900

    Adjust auto event scheduling and Firestore config

--- client/src/firebaseConfig.js
+++ client/src/firebaseConfig.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged, signInAnonymously } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeFirestore } from "firebase/firestore";
 // functions を利用するためにインポート
 import { getFunctions } from "firebase/functions";
 
@@ -16,7 +16,10 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app);
+const db = initializeFirestore(app, {
+  ignoreUndefinedProperties: true,
+  experimentalAutoDetectLongPolling: true,
+});
 // functions を初期化（リージョンはFunction側と合わせます）
 const functions = getFunctions(app, "asia-northeast1");
 
